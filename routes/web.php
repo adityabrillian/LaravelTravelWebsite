535c414e0d6cbe1df9a0a727a878011619c4ecb5
Rahmat Noris Adityabrillian <rahmatnoris@gmail.com>
2020-10-07 21:23:19 +0700

    DashboardController

--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,7 @@ Route::prefix('admin')
     ->namespace('Admin')
     ->middleware(['auth','admin'])
     ->group(function() {
-        Route::get('/','dashboardController@index')
+        Route::get('/','DashboardController@index')
             ->name('dashboard');
 
         Route::resource('user', 'UserController');
